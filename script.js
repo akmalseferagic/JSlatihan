fd466e13c0c0f7a664339ff5940d42871e46e2a2
akmalseferagic <akmalseferagic@gmail.com>
2022-02-21 23:55:27 +0700

    push

---
+++ script.js
@@ -0,0 +1,17 @@
+//OPERATOR ASIGMENT
+console.log("operator aritmatika")
+
+var operaMatika = 10 - 1
+var operaMatika2 = 10 * 2
+var operaMatika3 = 2 ** 10
+
+console.log("Hasilnya adalah : " + operaMatika)
+console.log(operaMatika2)
+console.log(operaMatika3)
+
+//OPERATOR ASIGMENT
+console.log("OPERATOR ASIGMENT")
+asigmentopera = 20
+asigmentopera %= 2
+
+console.log(asigmentopera)
