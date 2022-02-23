231414a2a0aa1f8becda805097babb1bf469e470
akmalseferagic <akmalseferagic@gmail.com>
2022-02-23 22:54:38 +0700

    mulai pusing

--- index.js
+++ index.js
@@ -3,3 +3,28 @@ function getText() {
 
     document.getElementsByTagName('p')[0].innerText = saveText
 }
+
+//POP UP
+let demos = document.getElementById("demos");
+demos.onclick = showMessage;
+
+function showMessage() {
+    alert("Hello, World!");
+}
+
+//Untuk mendapatkan text dalam kolom input text
+function getInput() {
+    const valueInput = document.getElementsByTagName('input')[0].value
+
+    document.getElementsByTagName('p')[1].innerText = valueInput
+}
+
+//Hello World memakai element.innerHTML
+let demo = document.getElementById("demo");
+
+console.log(demo.innerHTML); // Output: Hello, World!
+
+// Ubah konten <p id="demo"> menjadi Hello, Dunia!
+demo.innerHTML = "Hello, Dunia!";
+
+console.log(demo.innerHTML); // Output: Hello, Dunia!
